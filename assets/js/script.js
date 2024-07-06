91392332b0abbe9cc35e20e71fb4122245e3c002
William-figure <wyc19981230@outlook.com>
2024-07-06 23:53:43 +1000

    come back later, need to test the draggability

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ function generateTaskId() {
 function createTaskCard(task) {
   $("#todo-cards").append(
     `
-      <div class="task-card draggable card mb-3" id="task${task.taskId}">
+      <div style="z-index:0" class="task-card draggable card mb-3" id="task${task.taskId}">
         <div class="card-body">
           <h5 class="card-title">${task.taskName}</h5>
           <p class="card-text">Due: ${task.taskDueDate}</p>
@@ -34,7 +34,11 @@ function renderTaskList() {
   for (let i = 0; i < taskList.length; i++) {
     createTaskCard(taskList[i]);
   }
-  $(".task-card").draggable();
+  $(".draggable").draggable({
+    revert: true, // Snap back if not dropped on a droppable
+    cursor: "move", // Change cursor while dragging
+    containment: ".col-12 col-lg-4 d-flex" // Restrict dragging within document
+  });
   // $(".task-card").draggable({containment: "#todo-cards"});
 }
 
@@ -87,12 +91,13 @@ function handleDeleteTask(event) {
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-  $('#todo-cards').droppable({accept: ".task-card",
+  $(".droppable").droppable({
+    accept: ".draggable", // Only accept elements with class .draggable
     drop: function(event, ui) {
+        // Append the dragged element to the droppable container
         $(this).append(ui.draggable);
-    }});
-  $('#in-progress-cards').droppable({accept: "in-progress-cards"});
-  $('#done-cards').droppable({accept: "done-cards"});
+    }
+  });
 }
 
 //test the date input out only, since it is very crucial using date.js to format the date correctly, restrict user input as YYYY-MM-DD;
@@ -114,4 +119,5 @@ $(document).ready(function () {
   renderTaskList();
   handleAddTask();
   handleDeleteTask();
+  handleDrop();
 });
